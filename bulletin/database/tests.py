123e9ba5e04791f668f2342528c3aacfae4feb0b
Kirill Smirnov <smirnovk.1990@gmail.com>
2019-10-08 13:27:41 +0300

    mommy_model minor changes

--- bulletin/database/tests.py
+++ bulletin/database/tests.py
@@ -2,17 +2,16 @@ import datetime
 
 from django.utils import timezone
 from django.test import TestCase
+from model_mommy import mommy
 
 from .models import Car
 
 class CarModelTests(TestCase):
 
     def test_wrong_date_type(self):
-        """
-        returns False, if published date has wrong type
-        """
-        test_car = Car(pub_date = 'получаю какие-то данные' )
-        self.assertIs(type(test_car.pub_date) is datetime.datetime, True)
+       
+        test_car = mommy.make(Car)
+        self.assertTrue(isinstance(test_car, Car))
         
         
         
