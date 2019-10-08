2081a6b8240807c388118502b9981f75b58a0663
Kirill Smirnov <smirnovk.1990@gmail.com>
2019-10-08 14:27:17 +0300

    matching with current master

--- parsing/parser_auto.py
+++ parsing/parser_auto.py
@@ -1,27 +1,15 @@
-import requests
+from datetime import datetime
+
+import requests, time, csv
 from bs4 import BeautifulSoup
 
-def auto_parce():
-    print('hello')
-    #auto_url = "https://auto.ru/moskva/cars/volvo/used/"
-    auto_url = 'https://www.avito.ru/moskva_i_mo/avtomobili/avtomat/benzin/levyy_rul/ne_bolee_dvuh/ne_bityy'
-    '''params = {
-        "sort": "fresh_relevance_1-desc",
-        "year_from": 2014,
-        "year_to": 2019,
-        "engine_group": 'GASOLINE',
-        "transmission": "AUTOMATIC",
-        "displacement_to": 2800,
-        "displacement_from": 1800,
-        "pts_status": 1,
-        "steering_wheel": "LEFT",
-        "owners_count_group": "LESS_THAN_TWO",
-        "price_to": 1500000,
-        "price_from": 800000,
-        "km_age_to": 80000
-    }'''
+import parser_lib
+
+
+def auto_parce(url):
+    print('start parsing')
+    auto_url = url
     try:
-        #result = requests.get(auto_url, params=params)
         result = requests.get(auto_url)
         result.raise_for_status()
         return result.text
@@ -30,14 +18,12 @@ def auto_parce():
         print('Network error')
         return False
 
-def auto_item():
-    html = auto_parce()
+def all_auto_items(link_for_parce):
+    html = auto_parce(link_for_parce)
     if html:
         soup = BeautifulSoup(html, 'html.parser')
-        #soup = soup.prettify()
         all_cars = soup.findAll('a', class_="item-description-title-link")
         result_news = []
-        print(all_cars)
         for car in all_cars:
             title = car.find('span').text
             try:
@@ -45,14 +31,55 @@ def auto_item():
             except BaseException as e:
                 print(e)
 
-            #published = news.find('time').text
             result_news.append({
                 'title': title,
                 'url': url,
-                #'published': published
             })
         return result_news
     return False
 
+def auto_item():
+    auto_list = all_auto_items('https://www.avito.ru/moskva_i_mo/avtomobili/avtomat/benzin/levyy_rul/ne_bolee_dvuh/ne_bityy')
+    time.sleep(10)
+    car_full_info = []
+    for item in auto_list:
+        link = 'https://www.avito.ru'+ item['url']
+        item_info = auto_parce(link)
+
+        if item_info:
+            soup = BeautifulSoup(item_info, 'html.parser')
+
+            avito_item_number = parser_lib.get_item_number(soup)
+            title = parser_lib.get_title(soup)
+            published = datetime.now().strftime('%d.%m.%Y %H:%M')
+            price = parser_lib.get_price(soup)
+            seller = parser_lib.get_seller(soup)
+            phone = parser_lib.get_phone(soup)
+            description = parser_lib.get_description(soup)
+            all_specs = parser_lib.get_all_specs(soup)
+
+            car_full_info.append({
+                'avito_item_number': avito_item_number,
+                'title': title,
+                'published': published,
+                'price': price,
+                'seller': seller,
+                'phone': phone,
+                'description': description,
+                'car_specs': all_specs
+                })
+            print(car_full_info)
+            print('ok!')
+            time.sleep(180)     
+    return car_full_info
+
+
 if __name__ == '__main__':
-    print(auto_item())
+    result = auto_item()
+    print(result)
+    with open('export_cars.csv', 'w', encoding='utf-8', newline='') as f:
+        fields = ['title', 'published', 'price', 'seller', 'phone', 'description', 'car_specs']
+        writer = csv.DictWriter(f, fields, delimiter=';')
+        writer.writeheader()
+        for auto in result:
+            writer.writerow(auto)
