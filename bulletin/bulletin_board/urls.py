6eecd51c627a231e61630a5a87c3aa1f3ffc334b
Loshkarev Vadim <ipsorus@inbox.ru>
2019-10-22 00:59:23 +0300

    Немного изменил поля модели Car, добавил параметр индексации нужных полей, сделал вывод всех постов из бд на главной странице, а также подробное описание каждого объявления

--- bulletin/bulletin_board/urls.py
+++ bulletin/bulletin_board/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import *
 
+
 urlpatterns = [
-    path('', posts_list)
+    path('', posts_list, name='posts_list_url'),
+    path('post/<str:id>/', post_detail, name="post_detail_url")
 
 ]
--- bulletin/bulletin_board/views.py
+++ bulletin/bulletin_board/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from .models import Car
 # Create your views here.
+
 def posts_list(request):
-    return render(request,'bulletin_board/index.html')
+    posts = Car.objects.all()
+    return render(request,'bulletin_board/index.html', context={'posts': posts})
+
+
+def post_detail(request, id):
+    post = Car.objects.get(id__iexact=id)
+    return render(request, 'bulletin_board/post_detail.html', context={'post':post})
