9c073c79ce7b6bcb7d3ac27ba23b08dff41414e8
Loshkarev Vadim <ipsorus@inbox.ru>
2019-10-30 23:40:26 +0300

    Добавлены характеристики к авто в два столбца

--- bulletin/user/models.py
+++ bulletin/user/models.py
@@ -1,11 +1,11 @@
-from django.db import models
-from .forms import RegisterForm
-from bulletin_board.models import Car
+# from django.db import models
+# from .forms import RegisterForm
+# from bulletin_board.models import Car
 
-class FavoriteCars(models.Model):
+# class FavoriteCars(models.Model):
 
-    user = models.ForeignKey(RegisterForm,on_delete=models.SET_NULL)
-    car = models.ForeignKey(Car,on_delete=models.SET_NULL)
+#     user = models.ForeignKey(RegisterForm,on_delete=models.SET_NULL)
+#     car = models.ForeignKey(Car,on_delete=models.SET_NULL)
 
 
  
