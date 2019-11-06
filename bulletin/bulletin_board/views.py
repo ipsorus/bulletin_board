43099ff9b662d62fa9f9932151c9d778f0730347
Loshkarev Vadim <ipsorus@inbox.ru>
2019-11-06 17:14:48 +0300

    Добавление на сайт кнопок регистрации и авторизации

--- bulletin/bulletin_board/views.py
+++ bulletin/bulletin_board/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
 from django.views.generic import View
